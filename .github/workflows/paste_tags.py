d80b26012b1c6a3044895d5f73f88b8becab979e
pb222xd <121645898+pb222xd@users.noreply.github.com>
2024-05-12 04:40:13 +0800

    Update paste_tags.py

--- .github/workflows/paste_tags.py
+++ .github/workflows/paste_tags.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 import sys
 import json
-import base64
 
 if __name__ == '__main__':
     if len(sys.argv) == 3:
         metajson = json.loads(base64.b64decode(sys.argv[1]).decode('utf-8'))
         tagsList = metajson.get('tags', [])
-        deviceTagsList = base64.b64decode(sys.argv[2]).decode('utf-8').split('\n')
+        deviceTagsList = [tag for tag in base64.b64decode(sys.argv[2]).decode('utf-8').split('\n') if tag]
         combinedTags = []
         for tag in tagsList:
             for device in deviceTagsList:
