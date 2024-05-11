90665e3fdab608d0617650893042785c6b2b7879
pb222xd <121645898+pb222xd@users.noreply.github.com>
2024-05-12 04:43:12 +0800

    Update paste_tags.py

--- paste_tags.py
+++ paste_tags.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     if len(sys.argv) == 3:
         metajson = json.loads(base64.b64decode(sys.argv[1]).decode('utf-8'))
         tagsList = metajson.get('tags', [])
-        deviceTagsList = base64.b64decode(sys.argv[2]).decode('utf-8').split('\n')
+        deviceTagsList = [tag for tag in base64.b64decode(sys.argv[2]).decode('utf-8').split('\n') if tag]
         combinedTags = []
         for tag in tagsList:
             for device in deviceTagsList:
